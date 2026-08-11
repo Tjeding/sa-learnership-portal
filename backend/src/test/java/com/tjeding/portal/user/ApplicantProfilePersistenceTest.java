6d0a84402bf0206c7eb46225fa30406c9dbbad92
Tjeding <makubungtjeding@gmail.com>
2026-08-11 12:25:20 +0200

    lint changes at ApplicantProfilePersistenceTest.java

--- backend/src/test/java/com/tjeding/portal/user/ApplicantProfilePersistenceTest.java
+++ backend/src/test/java/com/tjeding/portal/user/ApplicantProfilePersistenceTest.java
@@ -2,7 +2,6 @@ package com.tjeding.portal.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
@@ -10,7 +9,6 @@ import org.springframework.test.context.ActiveProfiles;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("dev")
 class ApplicantProfilePersistenceTest {
 
